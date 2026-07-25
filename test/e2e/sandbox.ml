9762b195fcb5b9cc95e1248f20cb4c0f37504fc9
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-07-25 00:46:59 -0500

    Vibe out some tests

---
+++ test/e2e/sandbox.ml
@@ -0,0 +1,65 @@
+let read_all ic =
+  let buf = Buffer.create 256 in
+  (try
+     while true do
+       Buffer.add_channel buf ic 1
+     done
+   with End_of_file -> ());
+  Buffer.contents buf
+
+let run prog args =
+  let argv = Array.of_list (prog :: args) in
+  let stdout, stdin, stderr =
+    Unix.open_process_args_full prog argv (Unix.environment ())
+  in
+  close_out stdin;
+  let out = read_all stdout in
+  let err = read_all stderr in
+  let status = Unix.close_process_full (stdout, stdin, stderr) in
+  let code = match status with Unix.WEXITED c -> c | _ -> -1 in
+  (code, out, err)
+
+let git args = run "git" args
+let forkctl_exe = Filename.concat (Sys.getcwd ()) "../../bin/main.exe"
+let forkctl args = run forkctl_exe args
+
+let mktempdir () =
+  let base = Filename.temp_file "forkctl_e2e_" "" in
+  Sys.remove base;
+  Unix.mkdir base 0o700;
+  base
+
+let with_repo (f : unit -> unit) =
+  let dir = mktempdir () in
+  let prev = Sys.getcwd () in
+  Sys.chdir dir;
+  Fun.protect
+    ~finally:(fun () ->
+      Sys.chdir prev;
+      ignore (run "rm" [ "-rf"; dir ]))
+    (fun () ->
+      ignore (git [ "init"; "-q"; "-b"; "main" ]);
+      ignore (git [ "config"; "user.email"; "test@example.com" ]);
+      ignore (git [ "config"; "user.name"; "Test" ]);
+      ignore (git [ "commit"; "--allow-empty"; "-q"; "-m"; "upstream initial" ]);
+      f ())
+
+let rev_parse ref_ =
+  let code, out, _ = git [ "rev-parse"; ref_ ] in
+  if code <> 0 then None else Some (String.trim out)
+
+let show ref_path =
+  let code, out, _ = git [ "show"; ref_path ] in
+  if code <> 0 then None else Some out
+
+let parents_of commit =
+  let _, out, _ = git [ "log"; "-1"; "--pretty=%P"; commit ] in
+  String.split_on_char ' ' (String.trim out) |> List.filter (( <> ) "")
+
+let is_ancestor ~ancestor ~descendant =
+  let code, _, _ = git [ "merge-base"; "--is-ancestor"; ancestor; descendant ] in
+  code = 0
+
+let current_branch () =
+  let _, out, _ = git [ "symbolic-ref"; "--short"; "HEAD" ] in
+  String.trim out
---
+++ test/e2e/test_patch_stack_e2e.ml
@@ -0,0 +1,59 @@
+let test_new_patch_no_deps () =
+  Sandbox.with_repo (fun () ->
+      let upstream = Option.get (Sandbox.rev_parse "main") in
+      let code, _, err = Sandbox.forkctl [ "patch"; "new"; "foo" ] in
+      Alcotest.(check int) ("exit code: " ^ err) 0 code;
+      Alcotest.(check (option string))
+        "topic == upstream tip" (Some upstream)
+        (Sandbox.rev_parse "refs/heads/t/foo");
+      Alcotest.(check (option string))
+        "base == upstream tip" (Some upstream)
+        (Sandbox.rev_parse "refs/top-bases/foo");
+      Alcotest.(check (option string))
+        ".topdeps empty" (Some "")
+        (Sandbox.show "t/foo:.topdeps");
+      Alcotest.(check string)
+        "HEAD checked out onto topic" "t/foo" (Sandbox.current_branch ()))
+
+let test_new_patch_with_dep () =
+  Sandbox.with_repo (fun () ->
+      let upstream = Option.get (Sandbox.rev_parse "main") in
+      ignore (Sandbox.forkctl [ "patch"; "new"; "foo" ]);
+      let foo_tip = Option.get (Sandbox.rev_parse "refs/heads/t/foo") in
+      let code, _, err =
+        Sandbox.forkctl [ "patch"; "new"; "bar"; "--depends-on"; "foo" ]
+      in
+      Alcotest.(check int) ("exit code: " ^ err) 0 code;
+      Alcotest.(check (option string))
+        ".topdeps lists t/foo" (Some "t/foo")
+        (Sandbox.show "t/bar:.topdeps");
+      let base_bar = Option.get (Sandbox.rev_parse "refs/top-bases/bar") in
+      let parents = Sandbox.parents_of base_bar in
+      Alcotest.(check bool)
+        "base(bar) merges upstream" true (List.mem upstream parents);
+      Alcotest.(check bool)
+        "base(bar) merges t/foo" true (List.mem foo_tip parents);
+      Alcotest.(check bool)
+        "t/bar descends from base(bar)" true
+        (Sandbox.is_ancestor ~ancestor:base_bar ~descendant:"refs/heads/t/bar"))
+
+let test_list_topo_order () =
+  Sandbox.with_repo (fun () ->
+      ignore (Sandbox.forkctl [ "patch"; "new"; "foo" ]);
+      ignore (Sandbox.forkctl [ "patch"; "new"; "bar"; "--depends-on"; "foo" ]);
+      ignore (Sandbox.forkctl [ "patch"; "new"; "baz"; "--depends-on"; "bar" ]);
+      let code, out, err = Sandbox.forkctl [ "patch"; "list" ] in
+      Alcotest.(check int) ("exit code: " ^ err) 0 code;
+      let lines = String.split_on_char '\n' (String.trim out) in
+      Alcotest.(check (list string)) "topo order" [ "foo"; "bar"; "baz" ] lines)
+
+let () =
+  Alcotest.run "forkctl-e2e"
+    [
+      ( "patch-stack",
+        [
+          ("new: no deps", `Quick, test_new_patch_no_deps);
+          ("new: with dep", `Quick, test_new_patch_with_dep);
+          ("list: topo order", `Quick, test_list_topo_order);
+        ] );
+    ]
